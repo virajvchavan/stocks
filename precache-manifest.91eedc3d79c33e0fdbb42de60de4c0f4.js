1ede9935f2539ae8afe87d603f502934657a0ead
virajvchavan <viraj@joshsoftware.com>
2018-12-03 23:57:30 +0530

    Updates

---
+++ precache-manifest.91eedc3d79c33e0fdbb42de60de4c0f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0dbf44c858f32cb08bb",
+    "url": "/stocks/static/css/main.b1ab62e1.chunk.css"
+  },
+  {
+    "revision": "a0dbf44c858f32cb08bb",
+    "url": "/stocks/static/js/main.a0dbf44c.chunk.js"
+  },
+  {
+    "revision": "dbd0e775fe636e1b0bce",
+    "url": "/stocks/static/css/1.98ed60c5.chunk.css"
+  },
+  {
+    "revision": "dbd0e775fe636e1b0bce",
+    "url": "/stocks/static/js/1.dbd0e775.chunk.js"
+  },
+  {
+    "revision": "27b899632dbab2084591",
+    "url": "/stocks/static/js/runtime~main.27b89963.js"
+  },
+  {
+    "revision": "9332abd4bf069cee4d78d3be1bc7d56b",
+    "url": "/stocks/index.html"
+  }
+];
